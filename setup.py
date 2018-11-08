35f825c4be8f87a52ed38ebf5519b01d5c15d028
Jochen Wersdoerfer <jochen@wersdoerfer.de>
2018-11-08 18:03:04 +0100

    reformatted setup.py

--- setup.py
+++ setup.py
@@ -14,47 +14,45 @@ def get_version(*file_paths):
     """Retrieves the version from cast/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
+    raise RuntimeError("Unable to find version string.")
 
 
 version = get_version("cast", "__init__.py")
 
 
-if sys.argv[-1] == 'publish':
+if sys.argv[-1] == "publish":
     try:
         import wheel
+
         print("Wheel version: ", wheel.__version__)
     except ImportError:
         print('Wheel library missing. Please run "pip install wheel"')
         sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
     sys.exit()
 
-if sys.argv[-1] == 'tag':
+if sys.argv[-1] == "tag":
     print("Tagging the version on git:")
     os.system("git tag -a %s -m 'version %s'" % (version, version))
     os.system("git push --tags")
     sys.exit()
 
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
 
 setup(
-    name='django-cast',
+    name="django-cast",
     version=version,
     description="""Just another blogging / podcasting package""",
-    long_description=readme + '\n\n' + history,
-    author='Jochen Wersdörfer',
-    author_email='jochen-djangocast@wersdoerfer.de',
-    url='https://github.com/ephes/django-cast',
-    packages=[
-        'cast',
-    ],
+    long_description=readme + "\n\n" + history,
+    author="Jochen Wersdörfer",
+    author_email="jochen-djangocast@wersdoerfer.de",
+    url="https://github.com/ephes/django-cast",
+    packages=["cast"],
     include_package_data=True,
     install_requires=[
         "Pillow",
@@ -65,14 +63,14 @@ setup(
     ],
     license="BSD",
     zip_safe=False,
-    keywords='django-cast',
+    keywords="django-cast",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django :: 2.0',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django :: 2.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
 )
