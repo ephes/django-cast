d7735313b1604ea2d0f9262d35b8921d8d35765f
Jochen Wersdoerfer <jochen@wersdoerfer.de>
2019-03-23 08:18:41 +0100

    fixec documentation generation

--- tests/settings.py
+++ tests/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "django.contrib.sessions",
     "django.contrib.staticfiles",
+    "django.contrib.admin",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
