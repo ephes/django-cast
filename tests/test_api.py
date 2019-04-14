bca64029106681aab0d895e9bc7b1a45eacc38fe
Jochen Wersdörfer <jochen@wersdoerfer.de>
2019-04-14 14:58:00 +0200

    added basic tests for audio endpoints

--- tests/test_api.py
+++ tests/test_api.py
@@ -108,3 +108,38 @@ class TestBlogVideo:
         # dont redirect to login page
         assert r.status_code == 200
         assert "results" in r.json()
+
+
+class TestBlogAudio:
+    @classmethod
+    def setup_class(cls):
+        cls.list_url = reverse("cast:api:audio_list")
+        cls.detail_url = reverse("cast:api:audio_detail", kwargs={"pk": 1})
+
+    @pytest.mark.django_db
+    def test_audio_list_endpoint_without_authentication(self, api_client):
+        """Check for not authenticated status code if trying to access the list
+        endpoint without being authenticated.
+        """
+        r = api_client.get(self.list_url, format="json")
+        assert r.status_code == 403
+
+    @pytest.mark.django_db
+    def test_audio_detail_endpoint_without_authentication(self, api_client):
+        """Check for not authenticated status code if trying to access the
+        detail endpoint without being authenticated.
+        """
+        r = api_client.get(self.detail_url, format="json")
+        assert r.status_code == 403
+
+    @pytest.mark.django_db
+    def test_audio_list_endpoint_with_authentication(self, api_client):
+        """Check for list result when accessing the list endpoint
+        being logged in.
+        """
+        user = UserFactory()
+        api_client.login(username=user.username, password="password")
+        r = api_client.get(self.list_url, format="json")
+        # dont redirect to login page
+        assert r.status_code == 200
+        assert "results" in r.json()
