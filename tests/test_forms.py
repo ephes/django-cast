fbcd8b1abd3469d2e412cdb3a38224be01d5593f
Jochen Wersdoerfer <jochen@wersdoerfer.de>
2019-04-15 15:32:49 +0200

    some chapter mark form tests

---
+++ tests/test_forms.py
@@ -0,0 +1,26 @@
+import pytest
+
+from cast.forms import ChapterMarkForm
+
+
+class TestChapterMarkForm:
+    @pytest.mark.django_db
+    def test_add_chapter_mark_form(self, audio):
+        row = {
+            "audio": audio.pk,
+            "start": "01:01:01.123",
+            "title": "foo bar baz",
+        }
+        form = ChapterMarkForm(row)
+        assert form.is_valid()
+
+    @pytest.mark.django_db
+    def test_add_chapter_mark_form_invalid_url(self, audio):
+        row = {
+            "audio": audio.pk,
+            "start": "01:01:01.123",
+            "title": "foo bar baz",
+            "link": "foobar",
+        }
+        form = ChapterMarkForm(row)
+        assert not form.is_valid()
