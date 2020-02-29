c36339f769237024afd950220a789fa58338d5dd
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-02-29 17:30:48 +0100

    remove media root after tests

--- runtests.py
+++ runtests.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals, absolute_import
 
 import os
 import sys
+import shutil
+
+from pathlib import Path
 
 import django
 from django.conf import settings
@@ -19,6 +22,8 @@ def run_tests(*test_args):
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(test_args)
+    media_root = Path(settings.MEDIA_ROOT)
+    shutil.rmtree(media_root)  # FIXME move cleanup to tests
     sys.exit(bool(failures))
 
 
