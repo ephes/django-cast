0fd01dfc6e5d436621cd115c9ee423e795de178d
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-03-04 16:10:51 +0100

    started example app

---
+++ example_site/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'fi6y_c!w#4+16srq_%z+(dj=7d8&5+reik+_171*=e8(0(157x'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
