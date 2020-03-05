302bf402551bd19870bb9aef27bab5ba28053bc0
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-03-05 15:26:43 +0100

    added body

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname="full"),
+    ]
