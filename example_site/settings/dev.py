c4b48cde609eae6a4cdefbb7be83f0e21940e098
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-03-29 11:15:50 +0200

    working settings + black

--- example_site/settings/dev.py
+++ example_site/settings/dev.py
@@ -4,16 +4,16 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fi6y_c!w#4+16srq_%z+(dj=7d8&5+reik+_171*=e8(0(157x'
+SECRET_KEY = "fi6y_c!w#4+16srq_%z+(dj=7d8&5+reik+_171*=e8(0(157x"
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ["*"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-INSTALLED_APPS.extend([
-    "django_extensions",
-])
+INSTALLED_APPS.extend(
+    ["django_extensions",]
+)
 
 
 try:
