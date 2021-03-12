f88764d66f15edfc53b379f51a4e7bf2b7cb3f12
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-03-12 07:50:50 +0100

    changes

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_site.settings.dev")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
