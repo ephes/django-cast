e84be06ba9baf033688b3d4ea2db6651c0d82d4b
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-08-15 07:13:56 +0200

    fixed blog index url

--- tests/test_post_list.py
+++ tests/test_post_list.py
@@ -37,7 +37,7 @@ class TestPostList:
         assert draft_post.title in content
 
     def test_get_post_list_without_post_detail(self, client, post):
-        blog_url = reverse("cast:post_list", kwargs={"slug": post.blog.slug})
+        blog_url = post.blog.get_url()
 
         r = client.get(blog_url)
         assert r.status_code == 200
