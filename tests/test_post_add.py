95c83daa460957058667f416a0acd88fb242505c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-08-18 12:18:11 +0200

    test adding body block with video adds video to post.videos

--- tests/test_post_add.py
+++ tests/test_post_add.py
@@ -70,31 +70,28 @@ class TestPostAdd:
         assert post.images.count() == 1
         assert post.images.first() == wagtail_image
 
+    def test_submit_add_form_post_authenticated_with_video(self, client, post_data_wagtail, blog, video):
+        _ = client.login(username=blog.owner.username, password=blog.owner._password)
+        add_url = reverse("wagtailadmin_pages:add", args=("cast", "post", blog.id))
+        post_data_wagtail["body-0-value-0-type"] = "video"
+        post_data_wagtail["body-0-value-0-value"] = video.id
+
+        r = client.post(add_url, post_data_wagtail)
+
+        # make sure we are redirected to blog index
+        assert r.status_code == 302
+        assert r.url == reverse("wagtailadmin_explore", args=(blog.id,))
+
+        post = Post.objects.get(slug=post_data_wagtail["slug"])
+
+        # make sure there was a post added to the database
+        assert post.title == post_data_wagtail["title"]
+
+        # make sure there was an video added
+        assert post.videos.count() == 1
+        assert post.videos.first() == video
+
     # FIXME test post with media in content -> db link between media and post later
-    # def test_post_create_authenticated_with_video(self, client, blog, video):
-    #     user = video.user
-    #     r = client.login(username=user.username, password=user._password)
-
-    #     content = "with video: {{% video {} %}}".format(video.pk)
-    #     create_url = reverse("cast:post_create", kwargs={"slug": blog.slug})
-    #     data = {
-    #         "title": "test title",
-    #         "content": content,
-    #         "published": True,
-    #         "keywords": "",
-    #         "podcast_audio": "",
-    #         "explicit": "2",  # 2 -> no
-    #         "block": False,
-    #         "slug": "blog-slug",
-    #     }
-    #     r = client.post(create_url, data)
-    #     assert r.status_code == 302
-    #     bp = Post.objects.get(slug=data["slug"])
-    #     bvs = list(bp.videos.all())
-    #     assert bp.title == data["title"]
-    #     assert len(bvs) == 1
-    #     assert bvs[0].pk == video.pk
-
     # def test_post_create_authenticated_with_gallery(self, client, blog, gallery):
     #     user = gallery.user
     #     r = client.login(username=user.username, password="password")
