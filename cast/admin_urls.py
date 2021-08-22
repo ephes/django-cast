48e9010b9b62800a12c50c4c105a8f2b62053368
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-08-22 14:00:24 +0200

    consistent prefix

--- cast/admin_urls.py
+++ cast/admin_urls.py
@@ -5,8 +5,8 @@ from .views import wagtail_video as video
 urlpatterns = [
     path("video", video.index, name="video_index"),
     path(r"video/add/", video.add, name="video_add"),
-    path("edit/<int:video_id>/", video.edit, name="video_edit"),
-    path("delete/<int:video_id>/", video.delete, name="video_delete"),
+    path("video/edit/<int:video_id>/", video.edit, name="video_edit"),
+    path("video/delete/<int:video_id>/", video.delete, name="video_delete"),
     path("video/chooser/", video.chooser, name="video_chooser"),
     path("video/chooser/upload/", video.chooser_upload, name="video_chooser_upload"),
     path("video/chooser/<int:video_id>/", video.chosen, name="video_chosen"),
