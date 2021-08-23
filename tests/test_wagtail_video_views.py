dc8a9a19bc92c96f237e52149ed401260fb9c5f6
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-08-23 11:38:19 +0200

    added VideoUrls helper + one tests checks for needed authentication on all wagtailadmin video endpoints

--- tests/test_wagtail_video_views.py
+++ tests/test_wagtail_video_views.py
@@ -3,24 +3,58 @@ import pytest
 from django.urls import reverse
 
 
-class TestVideoIndexNotAuthenticated:
-    index_url = reverse("castmedia:video_index")
+def get_endpoint_urls_without_args():
+    urls = {}
+    view_names = ["index", "add", "chooser", "chooser_upload"]
+    for view_name in view_names:
+        video_view_name = f"video_{view_name}"
+        urls[video_view_name] = reverse(f"castmedia:{video_view_name}")
+    return urls
 
-    def test_get_video_index_not_authenticated(self, client):
-        r = client.get(self.index_url)
 
-        # redirect to login
-        assert r.status_code == 302
-        login_url = reverse("wagtailadmin_login")
-        assert login_url in r.url
+def get_endpoint_urls_with_args(video):
+    urls = {}
+    view_names = ["edit", "delete", "chosen"]
+    for view_name in view_names:
+        video_view_name = f"video_{view_name}"
+        urls[video_view_name] = reverse(f"castmedia:{video_view_name}", args=(video.id,))
+    return urls
+
+
+class VideoUrls:
+    def __init__(self, video):
+        self.urls = get_endpoint_urls_without_args()
+        self.urls.update(get_endpoint_urls_with_args(video))
+
+    def __getattr__(self, item):
+        return self.urls[item]
+
+
+@pytest.fixture
+def video_urls(video):
+    return VideoUrls(video)
+
+
+class TestVideoEndpointsNotAuthenticated:
+    pytestmark = pytest.mark.django_db
+
+    def test_get_all(self, client, video_urls):
+
+        for view_name, url in video_urls.urls.items():
+            r = client.get(url)
+
+            # redirect to login
+            assert r.status_code == 302
+            login_url = reverse("wagtailadmin_login")
+            assert login_url in r.url
 
 
 class TestVideoIndex:
     pytestmark = pytest.mark.django_db
     index_url = reverse("castmedia:video_index")
 
-    def test_get_video_index(self, authenticated_client):
-        r = authenticated_client.get(self.index_url, follow=True)
+    def test_get_video_index(self, authenticated_client, video_urls):
+        r = authenticated_client.get(video_urls.video_index, follow=True)
 
         assert r.status_code == 200
         content = r.content.decode("utf-8")
@@ -34,13 +68,13 @@ class TestVideoAdd:
     pytestmark = pytest.mark.django_db
     add_url = reverse("castmedia:video_add")
 
-    def test_get_add_video(self, authenticated_client):
-        r = authenticated_client.get(self.add_url)
+    def test_get_add_video(self, authenticated_client, video_urls):
+        r = authenticated_client.get(video_urls.video_add)
 
         assert r.status_code == 200
         content = r.content.decode("utf-8")
         assert "Uploading…" in content
 
-    def test_post_add_video(self, authenticated_client):
-        r = authenticated_client.post(self.add_url)
-        assert False
+    # def test_post_add_video(self, authenticated_client):
+    #     r = authenticated_client.post(self.add_url)
+    #     assert False
