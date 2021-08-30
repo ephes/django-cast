5576f433f720cbf42b4dbe171aee37ba6d84bbf2
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-08-30 10:24:20 +0200

    fixed original image_ids are used instead of filtered ones bug

--- cast/models.py
+++ cast/models.py
@@ -564,16 +564,17 @@ def get_or_create_gallery(image_ids):
     candidate_images = WagtailImage.objects.filter(id__in=image_ids)  # FIXME filter permissions
     if candidate_images.count() == 0:
         return None
+    filtered_image_ids = [ci.id for ci in candidate_images]
     gallery_to_image_ids = {}
     # FIXME filter permissions - fetch only images / galleries that
     # this user has permission to view
-    candidate_galleries = Gallery.objects.filter(images__in=image_ids).prefetch_related("images")
+    candidate_galleries = Gallery.objects.filter(images__in=filtered_image_ids).prefetch_related("images")
     for gallery in candidate_galleries:
         gallery_to_image_ids[frozenset(i.id for i in gallery.images.all())] = gallery
-    gallery = gallery_to_image_ids.get(frozenset(image_ids))
+    gallery = gallery_to_image_ids.get(frozenset(filtered_image_ids))
     if gallery is None:
         gallery = Gallery.objects.create()
-        gallery.images.add(*image_ids)
+        gallery.images.add(*filtered_image_ids)
     return gallery
 
 
