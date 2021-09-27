00abd864334bcb0848b7b026c8cc59bac85eb746
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-09-27 18:02:49 +0200

    moved all page models into pages.py

--- cast/models/__init__.py
+++ cast/models/__init__.py
@@ -1,10 +1,8 @@
 from .audio import Audio, ChapterMark, sync_chapter_marks
-from .blog import Blog
 from .file import File
 from .gallery import Gallery, get_or_create_gallery
-from .home import HomePage
 from .itunes import ItunesArtWork
-from .post import Post, sync_media_ids
+from .pages import Blog, HomePage, Post, sync_media_ids
 from .request import Request
 from .video import Video, get_video_dimensions
 
