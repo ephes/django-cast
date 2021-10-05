8d040430729cc3135e5bbb8871c9e8089594d7c1
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-05 11:06:15 +0200

    Added NaiveBayes spamfilter model + some tests

--- cast/models/moderation.py
+++ cast/models/moderation.py
@@ -1,6 +1,74 @@
+import re
+
+from collections import defaultdict
+
 from django.db import models
 
 from model_utils.models import TimeStampedModel
+
+
+token_pattern = re.compile(r"(?u)\b\w\w+\b")
+standard_tokenizer = token_pattern.findall
+
+
+def regex_tokenize(message):
+    return standard_tokenizer(message.lower())
+
+
+class NaiveBayes:
+    def __init__(self, tokenize=regex_tokenize):
+        self.tokenize = tokenize
+
+    def get_label_counts(self, messages):
+        label_counts = defaultdict(int)
+        for label, text in messages:
+            label_counts[label] += 1
+        return label_counts
+
+    def get_prior_probabilities(self, label_counts):
+        number_of_messages = sum(label_counts.values())
+        return {label: count / number_of_messages for label, count in label_counts.items()}
+
+    def get_word_label_counts(self, messages):
+        counts = defaultdict(lambda: defaultdict(int))
+        for label, text in messages:
+            for word in self.tokenize(text):
+                counts[word][label] += 1
+        return counts
+
+    def get_number_of_words(self, word_label_counts):
+        number_of_words = defaultdict(int)
+        for word, counts in word_label_counts.items():
+            for label, count in counts.items():
+                number_of_words[label] += 1
+        return number_of_words
+
+    def fit(self, messages):
+        self.prior_probabilities = self.get_prior_probabilities(self.get_label_counts(messages))
+        self.word_label_counts = self.get_word_label_counts(messages)
+        self.number_of_words = self.get_number_of_words(self.word_label_counts)
+        return self
+
+    def update_probabilities(self, probabilities, counts_per_label, number_of_words):
+        updated_probabilites = {}
+        for label, prior_probability in probabilities.items():
+            word_count = counts_per_label.get(label, 0.5)
+            word_probability = word_count / number_of_words[label]
+            updated_probabilites[label] = prior_probability * word_probability
+        return updated_probabilites
+
+    def predict(self, message):
+        probabilities = dict(self.prior_probabilities)
+        for word in self.tokenize(message):
+            counts_per_label = self.word_label_counts.get(word, {})
+            probabilities = self.update_probabilities(probabilities, counts_per_label, self.number_of_words)
+        return probabilities
+
+    def predict_label(self, message):
+        probabilities = self.predict(message)
+        if len(probabilities) == 0:
+            return None
+        return sorted(((prob, label) for label, prob in probabilities.items()), reverse=True)[0][1]
 
 
 class SpamFilter(TimeStampedModel):
---
+++ tests/test_moderation.py
@@ -0,0 +1,65 @@
+import pytest
+
+from cast.models.moderation import NaiveBayes
+
+
+@pytest.mark.parametrize(
+    "train, expected_initial_probabilities",
+    [
+        ([], {}),
+        ([("ham", ""), ("spam", "")], {"ham": 0.5, "spam": 0.5}),
+        ([(i, "") for i in range(3)], {k: 1 / 3 for k in range(3)}),
+    ],
+)
+def test_initial_probabilities(train, expected_initial_probabilities):
+    model = NaiveBayes().fit(train)
+    assert model.predict("") == expected_initial_probabilities
+
+
+@pytest.mark.parametrize(
+    "probabilities, counts_per_label, number_of_words, expected",
+    [
+        ({}, {}, {}, {}),
+        ({"spam": 1}, {"spam": 10}, {"spam": 100}, {"spam": 0.1}),
+        (
+            {"spam": 1, "ham": 0.5},
+            {"spam": 10, "ham": 5},
+            {"spam": 100, "ham": 50},
+            {"spam": 0.1, "ham": 0.05},
+        ),
+        # no word count -> make sure probability is > 0
+        ({"spam": 1}, {}, {"spam": 100}, {"spam": 0.005}),
+    ],
+)
+def test_predict_word_probabilities(probabilities, counts_per_label, number_of_words, expected):
+    model = NaiveBayes()
+    updated = model.update_probabilities(probabilities, counts_per_label, number_of_words)
+    assert updated == expected
+
+
+@pytest.mark.parametrize(
+    "message, expected_probabilities",
+    [("foo", {"ham": (1 / 3) * (0.5 / 2), "spam": (2 / 3) * (2 / 5)})],
+)
+def test_predict(message, expected_probabilities):
+    train = [
+        ("spam", "foo bar baz"),
+        ("spam", "foo asdf bsdf"),
+        ("ham", "asdf csdf"),
+    ]
+    model = NaiveBayes().fit(train)
+    probabilities = model.predict(message)
+    assert probabilities == expected_probabilities
+
+
+@pytest.mark.parametrize(
+    "train, message, expected_label",
+    [
+        ([], "asdf", None),
+        ([("spam", "foo bar baz")], "asdf", "spam"),
+    ],
+)
+def test_predict_label(train, message, expected_label):
+    model = NaiveBayes().fit(train)
+    label = model.predict_label(message)
+    assert label == expected_label
