3b1688cfcc2f9d7bff3c9a9e9a6efe7e45d42c65
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-09 08:12:15 +0200

    moved notebook configuration stuff in manage.py

--- example/manage.py
+++ example/manage.py
@@ -5,7 +5,17 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_site.settings.dev")
+    # handle DJANGO_SETTINGS_MODULE
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_site.settings.dev")
+    os.environ["DJANGO_SETTINGS_MODULE"] = "example_site.settings.dev"
+
+    # Are we starting a notebook server? -> DJANGO_ALLOW_ASYNC_UNSAFE=true
+    # chdir to notebooks
+    if "--notebook" in set(sys.argv):
+        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+        print("starting a notebook server")
+        os.chdir("notebooks")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
