e13b27c69cfaab99d8fa82362cb8b60be97aa59f
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-10 08:07:52 +0200

    got finally rid of all symlinks

--- example/manage.py
+++ example/manage.py
@@ -4,17 +4,24 @@ import os
 import sys
 
 
+def prepare_notebook_environment():
+    from pathlib import Path
+
+    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+    os.chdir("notebooks")
+    example_project_path = Path(__file__).resolve().parent
+    os.environ["PYTHONPATH"] = str(example_project_path)
+
+
 if __name__ == "__main__":
     # handle DJANGO_SETTINGS_MODULE
     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_site.settings.dev")
     os.environ["DJANGO_SETTINGS_MODULE"] = "example_site.settings.dev"
 
     # Are we starting a notebook server? -> DJANGO_ALLOW_ASYNC_UNSAFE=true
-    # chdir to notebooks
+    # chdir to notebooks and add example project to pythonpath
     if "--notebook" in set(sys.argv):
-        os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-        print("starting a notebook server")
-        os.chdir("notebooks")
+        prepare_notebook_environment()
 
     from django.core.management import execute_from_command_line
 
