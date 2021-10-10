f1d5d15fadd4974cecf1cd4ccb305876b5c10ce4
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-10-10 08:16:21 +0200

    fixed notebook start command \o/

--- commands.py
+++ commands.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import webbrowser
 
@@ -134,8 +133,5 @@ def docs():
 
 @typer_cli
 def notebook():
-    # FIXME does not work atm, see command_lines.txt
-    env = os.environ.copy()
-    env["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-    command = "python ../manage.py shell_plus --settings example_site.settings.dev --notebook"
-    run_command(command, cwd="notebooks", debug=True, env=env)
+    command = "python example/manage.py shell_plus --notebook"
+    run_command(command, debug=True)
