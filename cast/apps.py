251d02b4ea1cae80c97366ab56fb1a25cb42af73
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-11-13 07:51:48 +0100

    #43 added DELETE_WAGTAIL_IMAGES setting

--- cast/apps.py
+++ cast/apps.py
@@ -1,8 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_delete
+from wagtail.images import get_image_model
+from wagtail.images.signal_handlers import post_delete_file_cleanup
+
+from . import appsettings
 
 
 class CastConfig(AppConfig):
     name = "cast"
 
     def ready(self):
-        pass
+        if not appsettings.DELETE_WAGTAIL_IMAGES:
+            # Have a way to deactivate wagtails post_delete_file_cleanup
+            # which deletes the file physically when developing against S3
+            post_delete.disconnect(post_delete_file_cleanup, sender=get_image_model())
--- cast/appsettings.py
+++ cast/appsettings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-
 CAST_COMMENTS_ENABLED = getattr(settings, "CAST_COMMENTS_ENABLED", False)
 CHOOSER_PAGINATION = getattr(settings, "CHOOSER_PAGINATION", 10)
 MENU_ITEM_PAGINATION = getattr(settings, "MENU_ITEM_PAGINATION", 20)
 POST_LIST_PAGINATION = getattr(settings, "POST_LIST_PAGINATION", 5)
+DELETE_WAGTAIL_IMAGES = getattr(settings, "DELETE_WAGTAIL_IMAGES", True)
