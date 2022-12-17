fc15ab4e84cf16b2c50d200484f82cb4fb35ebbb
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-12-17 14:08:18 +0100

    #56 move docs to docs_old

--- docs_old/conf.py
+++ docs_old/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -26,7 +25,6 @@ sys.path.append(parent)
 import sphinx_rtd_theme
 
 import cast
-
 
 # -- General configuration -----------------------------------------------------
 
