9436b28047d48a61d7c5dcc8c5a849e8e0a1d227
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-01-05 09:34:00 +0100

    #50 some fixes after merge

--- cast/models/__init__.py
+++ cast/models/__init__.py
@@ -2,7 +2,7 @@ from .audio import Audio, ChapterMark, sync_chapter_marks
 from .file import File
 from .gallery import Gallery, get_or_create_gallery
 from .itunes import ItunesArtWork
-from .moderation import SpamFilter, comment_to_message, get_training_data_from_comments
+from .moderation import SpamFilter
 from .pages import Blog, HomePage, Post, sync_media_ids
 from .request import Request
 from .video import Video, get_video_dimensions
@@ -23,6 +23,4 @@ __all__ = [
     SpamFilter,
     Video,
     get_video_dimensions,
-    comment_to_message,
-    get_training_data_from_comments,
 ]
