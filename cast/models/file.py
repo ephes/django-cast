6790cd067365c799deecaf56c58957f7667911f9
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-02-06 23:24:34 +0100

    #57 annotations for file.py

--- cast/models/file.py
+++ cast/models/file.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-
 from model_utils.models import TimeStampedModel
 
 
@@ -8,7 +7,7 @@ class File(TimeStampedModel):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     original = models.FileField(upload_to="cast_files/")
 
-    def get_all_paths(self):
+    def get_all_paths(self) -> set[str]:
         paths = set()
         paths.add(self.original.name)
         return paths
