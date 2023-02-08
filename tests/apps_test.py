888db335acf48777f01675f94e7a1d66ebaddf28
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-02-08 16:51:37 +0100

    apps.py coverage at 100%

---
+++ tests/apps_test.py
@@ -0,0 +1,17 @@
+from django.apps import apps
+
+
+def test_delete_wagtail_images_app_setting(mocker):
+    cast_config = apps.get_app_config("cast")
+
+    # delete images True -> do not disconnect post_delete_file_cleanup
+    mocker.patch("cast.apps.appsettings.DELETE_WAGTAIL_IMAGES", True)
+    mocked_disconnect = mocker.patch("cast.apps.post_delete.disconnect")
+    cast_config.ready()
+    assert mocked_disconnect.call_count == 0
+
+    # delete images False -> disconnect post_delete_file_cleanup
+    mocker.patch("cast.apps.appsettings.DELETE_WAGTAIL_IMAGES", False)
+    mocked_disconnect = mocker.patch("cast.apps.post_delete.disconnect")
+    cast_config.ready()
+    assert mocked_disconnect.call_count == 1
