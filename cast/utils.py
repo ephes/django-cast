4a9eb86fe650a1b72c627f3f8012ad638adb48b0
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-02-08 07:57:46 +0100

    #57 annotations for utils

--- cast/utils.py
+++ cast/utils.py
@@ -1,12 +1,15 @@
 import os
+from collections.abc import Iterable
+
+from django.core.files.storage import FileSystemStorage
 
 
-def storage_walk_paths(storage, cur_dir=""):
+def storage_walk_paths(storage: FileSystemStorage, cur_dir: str = "") -> Iterable[str]:
     dirs, files = storage.listdir(cur_dir)
     for directory in dirs:
         new_dir = os.path.join(cur_dir, directory)
         for path in storage_walk_paths(storage, cur_dir=new_dir):
             yield path
-    for fname in files:
-        path = os.path.join(cur_dir, fname)
+    for file_name in files:
+        path = os.path.join(cur_dir, file_name)
         yield path
