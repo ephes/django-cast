cb94d39fa5be8e5201af4408c38522828a1da743
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-02-08 07:05:50 +0100

    #57 added annotations for filters..

--- cast/filters.py
+++ cast/filters.py
@@ -1,54 +1,62 @@
 import string
+from collections.abc import Iterable, Mapping
 from datetime import datetime
-from itertools import chain
+from typing import Any, Optional, cast
 
 import django_filters
+from django.core.files.uploadedfile import UploadedFile
 from django.db import models
 from django.db.models.fields import BLANK_CHOICE_DASH
+from django.db.models.functions import TruncMonth
 from django.forms import Widget
+from django.forms.renderers import BaseRenderer
 from django.forms.utils import flatatt
+from django.http import QueryDict
+from django.utils.datastructures import MultiValueDict
 from django.utils.encoding import force_str
 from django.utils.http import urlencode
-from django.utils.safestring import mark_safe
+from django.utils.safestring import SafeText, mark_safe
 from django.utils.translation import gettext as _
+from wagtail.models import PageQuerySet
 
 
 class DateFacetWidget(Widget):
-    def __init__(self, attrs=None, choices=()):
+    def __init__(self, attrs: Optional[dict[str, str]] = None):
         super().__init__(attrs)
+        self.choices: list[tuple[str, str]] = []
 
-        self.choices = choices
-
-    def value_from_datadict(self, data, files, name):
+    def value_from_datadict(
+        self, data: Mapping[str, Iterable[Any]], files: MultiValueDict[str, UploadedFile], name: str
+    ) -> str:
         value = super().value_from_datadict(data, files, name)
-        self.data = data
+        self.data: QueryDict = cast(QueryDict, data)
         return value
 
-    def render(self, name, value, attrs=None, choices=(), renderer=None):
-        if not hasattr(self, "data"):
-            self.data = {}
+    def render(
+        self, name: str, value: Any, attrs: Optional[dict[str, Any]] = None, renderer: Optional[BaseRenderer] = None
+    ) -> SafeText:
         if value is None:
             value = ""
         final_attrs = self.build_attrs(self.attrs, extra_attrs=attrs)
         output = ["<div%s>" % flatatt(final_attrs)]
-        options = self.render_options(choices, [value], name)
+        options = self.render_options([value], name)
         if options:
             output.append(options)
         output.append("</div>")
         return mark_safe("\n".join(output))
 
-    def render_options(self, choices, selected_choices, name):
-        selected_choices = {force_str(v) for v in selected_choices}
+    def render_options(self, selected_choices: list[str], name: str) -> str:
+        selected_choices_set = {force_str(v) for v in selected_choices}
         output = []
-        for option_value, option_label in chain(self.choices, choices):
+        for option_value, option_label in self.choices:
             if isinstance(option_label, (list, tuple)):
                 for option in option_label:
-                    output.append(self.render_option(name, selected_choices, *option))
+                    output.append(self.render_option(name, selected_choices_set, *option))
             else:
-                output.append(self.render_option(name, selected_choices, option_value, option_label))
+                output.append(self.render_option(name, selected_choices_set, option_value, option_label))
         return "\n".join(output)
 
-    def render_option(self, name, selected_choices, option_value, option_label):
+    def render_option(self, name: str, selected_choices: set[str], option_value: str, option_label: str) -> str:
         option_value = force_str(option_value)
         if option_label == BLANK_CHOICE_DASH[0][1]:
             option_label = _("All")
@@ -59,17 +67,19 @@ class DateFacetWidget(Widget):
             url = data.urlencode()
         except AttributeError:
             url = urlencode(data)
-        return self.option_string() % {
+        option_string = self.option_string()
+        return option_string % {
             "attrs": selected and ' class="selected"' or "",
             "query_string": url,
             "label": force_str(option_label),
         }
 
-    def option_string(self):
+    @staticmethod
+    def option_string() -> str:
         return '<div class="cast-date-facet-item"><a%(attrs)s href="?%(query_string)s">%(label)s</a></div>'
 
 
-def parse_date_facets(value):
+def parse_date_facets(value: str) -> datetime:
     """Split into function, because it needs to be imported by the post list view."""
     # clean up value a bit, because otherwise sql-injection
     # search requests are spamming the logfile with garbage -> analytics won't work
@@ -79,26 +89,31 @@ def parse_date_facets(value):
     return year_month
 
 
-def get_selected_facet(get_params):
+def get_selected_facet(get_params: dict) -> Optional[datetime]:
     date_facet = get_params.get("date_facets")
     if date_facet is None or len(date_facet) == 0:
         return None
     return parse_date_facets(date_facet)
 
 
-def get_facet_counts(filterset_data, queryset):
-    filterset_data = {k: v for k, v in filterset_data.items()}  # copy filterset_data to avoid overwriting
+def get_facet_counts(
+    filterset_data_orig: Optional[dict], queryset: Optional[models.QuerySet]
+) -> dict[str, dict[datetime, int]]:
+    if filterset_data_orig is None:
+        filterset_data = {}
+    else:
+        filterset_data = {k: v for k, v in filterset_data_orig.items()}  # copy filterset_data to avoid overwriting
 
     # get selected facet if set and build the facet counting queryset
     facet_counts = {}
     selected_facet = get_selected_facet(filterset_data)
     if selected_facet is not None:
         facet_counts = {"year_month": {selected_facet: 1}}
-    filterset_data["facet_counts"] = facet_counts
+    filterset_data["facet_counts"] = facet_counts  # type: ignore
     post_filter = PostFilterset(queryset=queryset, data=filterset_data, facet_counts=facet_counts)
     facet_queryset = (
         post_filter.qs.order_by()
-        .annotate(month=models.functions.TruncMonth("visible_date"))
+        .annotate(month=TruncMonth("visible_date"))
         .values("month")
         .annotate(n=models.Count("pk"))
     )
@@ -114,7 +129,7 @@ class FacetChoicesMixin:
     """Just a way to pass the facet counts to the field which displays the choice."""
 
     @property
-    def field(self):
+    def field(self):  # FIXME add type annotation
         facet_count_choices = []
         for year_month, count in sorted(self.parent.facet_counts.get("year_month", {}).items()):
             date_str = year_month.strftime("%Y-%m")
@@ -125,7 +140,7 @@ class FacetChoicesMixin:
 
 
 class DateFacetFilter(FacetChoicesMixin, django_filters.filters.ChoiceFilter):
-    def filter(self, qs, value):
+    def filter(self, qs: models.QuerySet, value: str) -> models.QuerySet:
         if len(value) == 0:
             # don't filter if value is empty
             return qs
@@ -158,11 +173,11 @@ class PostFilterset(django_filters.FilterSet):
 
     def __init__(
         self,
-        data=None,
-        queryset=None,
+        data: Optional[dict] = None,
+        queryset: Optional[models.QuerySet] = None,
         *,
-        facet_counts={},
-        fetch_facet_counts=False,
+        facet_counts: dict = {},
+        fetch_facet_counts: bool = False,
     ):
         super().__init__(data=data, queryset=queryset)
         self.facet_counts = facet_counts
@@ -176,5 +191,5 @@ class PostFilterset(django_filters.FilterSet):
                 self.facet_counts = {}
 
     @staticmethod
-    def fulltext_search(queryset, name, value):
+    def fulltext_search(queryset: PageQuerySet, _name: str, value: str) -> models.QuerySet:
         return queryset.search(value).get_queryset()
