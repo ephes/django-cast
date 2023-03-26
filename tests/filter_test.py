9411d498fe917e10f5ac30a5edf3ccf25f3bd2bc
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-03-26 21:23:43 +0200

    #85 fixed test

--- tests/filter_test.py
+++ tests/filter_test.py
@@ -40,4 +40,4 @@ def test_selected_date_facet_is_in_hidden_input():
     dfw = DateFacetWidget()
     dfw.data = QueryDict("date_facets=2018-12")
     option = dfw.render_option("date_facets", {"2018-12"}, "2018-12", "2018-12 (3)")
-    assert '<input type="hidden" name="date_facets" value="2018-12" />' in option
+    assert '<input type="hidden" name="date_facets" value="2018-12">' in option
