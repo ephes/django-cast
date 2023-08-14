972f9da11efd52a9ede2e394362d275ddfb961ff
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-08-14 10:19:12 +0200

    #99 if _files is a class variable, save adds to both production and backup -> fixed

--- cast/management/commands/media_backup.py
+++ cast/management/commands/media_backup.py
@@ -10,11 +10,14 @@ class Command(BaseCommand):
         "(requires Django >= 4.2 and production and backup storage configured)"
     )
 
-    def handle(self, *args, **options):
-        production_storage, backup_storage = get_production_and_backup_storage()
+    @staticmethod
+    def backup_media_files(production_storage, backup_storage):
         for num, path in enumerate(storage_walk_paths(production_storage)):
             if not backup_storage.exists(path):
                 with production_storage.open(path, "rb") as in_f:
                     backup_storage.save(path, in_f)
             if num % 100 == 0:
                 print(".", end="", flush=True)
+
+    def handle(self, *args, **options):
+        self.backup_media_files(*get_production_and_backup_storage())
