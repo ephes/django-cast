eb296f0a6300c85d8e6f82af44c0b4936ff99171
Jochen Wersdörfer <jochen@wersdoerfer.de>
2023-08-29 08:43:07 +0200

    #100 add a snippet view to be able to do crud on tags

--- cast/wagtail_hooks.py
+++ cast/wagtail_hooks.py
@@ -2,9 +2,13 @@ from django.http import HttpRequest
 from django.urls import include, path, reverse
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
+from taggit.models import Tag
 from wagtail import hooks
 from wagtail.admin.menu import MenuItem
+from wagtail.admin.panels import FieldPanel
 from wagtail.permission_policies.collections import CollectionOwnershipPermissionPolicy
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
 
 from .admin_urls import audio, video
 from .models import Audio, Video
@@ -74,3 +78,22 @@ def editor_js_audio() -> str:
         """,
         reverse("castaudio:chooser"),
     )
+
+
+class TagsSnippetViewSet(SnippetViewSet):
+    """
+    This is a snippet viewset for the taggit Tag model. It is used to
+    be able to do crud operations on tags from the admin menu.
+    """
+
+    panels = [FieldPanel("name")]  # only show the name field
+    model = Tag
+    icon = "tag"  # change as required
+    add_to_admin_menu = True
+    menu_label = "Tags"
+    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
+    list_display = ["name", "slug"]
+    search_fields = ("name",)
+
+
+register_snippet(TagsSnippetViewSet)
