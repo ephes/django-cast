35065f9ad585cb26f43ca6792927ad46d6de41ca
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-03-23 06:18:11 +0100

    #124 re-add feed cache

--- cast/urls.py
+++ cast/urls.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.urls import include, path
+from django.views.decorators.cache import cache_page
 
 from . import feeds
 from .views import meta
@@ -14,17 +15,17 @@ urlpatterns: list[Any] = [
     # Feeds
     path(
         "<slug:slug>/feed/rss.xml",
-        view=feeds.LatestEntriesFeed(),
+        view=cache_page(5 * 60)(feeds.LatestEntriesFeed()),
         name="latest_entries_feed",
     ),
     path(
         "<slug:slug>/feed/podcast/<audio_format>/rss.xml",
-        view=feeds.RssPodcastFeed(),
+        view=cache_page(5 * 60)(feeds.RssPodcastFeed()),
         name="podcast_feed_rss",
     ),
     path(
         "<slug:slug>/feed/podcast/<audio_format>/atom.xml",
-        view=feeds.AtomPodcastFeed(),
+        view=cache_page(5 * 60)(feeds.AtomPodcastFeed()),
         name="podcast_feed_atom",
     ),
     # Meta views like twitter player cards etc
