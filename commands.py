3d84847415d2594f96f2e1c9508eaf29fb0f06ba
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-06-27 12:05:44 +0200

    #154 new command to output the complete source with annotated file names

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 import platform
 import subprocess
@@ -122,6 +123,54 @@ def docs():
         subprocess.call(command)
     file_url = "file://" + str(Path("docs/_build/html/index.html").resolve())
     webbrowser.open_new_tab(file_url)
+
+
+@cli.command()
+def llm_content():
+    """
+    Output all relevant code / documentation in the project including
+    the relative path and content of each file.
+    """
+
+    def echo_filename_and_content(files):
+        """Print the relative path and content of each file."""
+        for f in files:
+            print(f)
+            contents = f.read_text()
+            relative_path = f.relative_to(project_root)
+            print(relative_path)
+            print("---")
+            print(contents)
+            print("---")
+
+    project_root = Path(get_project_root())
+    # Exclude files and directories. This is tuned to make the project fit into the
+    # 200k token limit of the claude 3 models.
+    exclude_files = {"bootstrap.min.js", "htmx.min.js", "jquery-3.7.1.min.js", "embed.5.js"}
+    exclude_dirs = {
+        ".tox",
+        "migrations",
+        "node_modules",
+        "_build",
+        "example",
+        "vite",
+        "htmlcov",
+        "releases",
+        "wagtail",
+        "management",
+    }
+    patterns = ["*.py", "*.rst", "*.js", "*.ts", "*.html"]
+    all_files = []
+    for root, dirs, files in os.walk(project_root):
+        root = Path(root)
+        # d is the plain directory name
+        dirs[:] = [d for d in dirs if d not in exclude_dirs]
+        for pattern in patterns:
+            for filename in fnmatch.filter(files, pattern):
+                if filename not in exclude_files:
+                    all_files.append(root / filename)
+    # print("\n".join([str(f) for f in all_files]))
+    echo_filename_and_content(all_files)
 
 
 if __name__ == "__main__":
