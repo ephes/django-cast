58eeed814178cd7461492955c24f07f5cfaa7eae
Jochen Wersdörfer <jochen@wersdoerfer.de>
2025-10-12 06:02:52 +0200

    Bump version to 0.2.49

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Django Cast"
 copyright = "2025, Jochen Wersdörfer"
 author = "Jochen Wersdörfer"
-release = "0.2.48"
+release = "0.2.49"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
