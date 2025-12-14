7768f04bd18e0226b1c5efb3162658e6e39c67cc
Jochen Wersdörfer <jochen@wersdoerfer.de>
2025-12-14 22:28:54 +0100

    Add proxy model migration for cast_comments

--- src/cast/comments/migrations/0001_initial.py
+++ src/cast/comments/migrations/0001_initial.py
@@ -4,6 +4,20 @@ from django.db import migrations
 class Migration(migrations.Migration):
     initial = True
 
-    dependencies: list[tuple[str, str]] = []
+    dependencies = [
+        ("threadedcomments", "0001_initial"),
+    ]
 
-    operations: list[migrations.operations.base.Operation] = []
+    operations = [
+        migrations.CreateModel(
+            name="CastComment",
+            fields=[],
+            options={
+                "verbose_name": "Comment",
+                "verbose_name_plural": "Comments",
+                "managed": False,
+                "proxy": True,
+            },
+            bases=("threadedcomments.threadedcomment",),
+        ),
+    ]
