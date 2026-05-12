aa2d0b58b69365bac65487b10b2d04282486f514
Jochen Wersdörfer <jochen@wersdoerfer.de>
2026-05-12 14:50:56 +0200

    Start 0.2.57 development

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Django Cast"
 copyright = "2025, Jochen Wersdörfer"
 author = "Jochen Wersdörfer"
-release = "0.2.56"
+release = "0.2.57"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
